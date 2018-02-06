8627792ea1ddc79f64df71e8a6ca79c519609382
Sarah Lee Jara <sarahlee.jara@gmail.com>
2018-02-06 16:26:48 +0100

    database shop features

--- js/movies.js
+++ js/movies.js
@@ -1,26 +1,40 @@
-var movies = [
+[
   {
-    "trailer" : "https://www.youtube.com/embed/h6DOpfJzmo0",
-    "movie title" : "Batman Movie",
+    "title" : "",
+    "trailer" : "",
   }
   {
+    "title" : "Lord of the Ring: The Two Towers",
     "trailer" : "https://www.youtube.com/embed/r5X-hFf6Bwo",
-    "movie title" : "Lord of the Ring: The Two Towers",
   }
   {
+    "title" : "Inception",
     "trailer" : "https://www.youtube.com/embed/8hP9D6kZseM",
-    "movie title" : "Inception",
   }
   {
+    "title" : "Intouchables",
     "trailer" : "https://www.youtube.com/embed/NXR6Whv9WBA",
-    "movie title" : "Intouchables",
   }
   {
+    "title" : "The English Patient",
     "trailer" : "https://www.youtube.com/embed/N1OopmufwUo",
-    "movie title" : "The English Patient",
   }
   {
+    "title" : "King Arthur",
     "trailer" : "https://www.youtube.com/embed/jIM4-HLtUM0",
-    "movie title" : "King Arthur",
+  }
+  {
+    "title" : "The Mummy",
+    "genre" : "Thriller/Action",
+    "year" : "2017",
+    "tralier" : "https://www.youtube.com/watch?v=GzorZUuZqEI",
+  }
+  {
+    "title" : "The Dark Tower",
+    "trailer" : "https://www.youtube.com/embed/GjwfqXTebIY",
+  }
+  {
+    "title" : "The maze runner 3"
+    "trailer" : "https://www.youtube.com/watch?v=4-BTxXm8KSg",
   }
 ]
---
+++ js/shop.js
@@ -0,0 +1,10 @@
+[
+  {
+    "title" : "The Dark Tower",
+    "trailer" : "https://www.youtube.com/embed/GjwfqXTebIY",
+    "story" : "Story line: Roland Deschain (Idris Elba), the last Gunslinger, is locked in an eternal battle with Walter O'Dim (Matthew McConaughey), also known as the Man in Black. The Gunslinger must prevent the Man in Black from toppling the Dark Tower, the key that holds the universe together. With the fate of worlds at stake, two men collide in the ultimate battle between good and evil.",
+    "date" : "August 23, 2017",
+    "genre" : "Fantasy/Science fiction",
+    "price" : "15,00€",
+  }
+]
